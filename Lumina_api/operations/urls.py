d6b7d2912596105788265ef238e6608d3c55a239
Alan-AW <xcdh560@foxmail.com>
2023-07-05 11:32:03 +0800

    url同步

--- Lumina_api/operations/urls.py
+++ Lumina_api/operations/urls.py
@@ -3,8 +3,8 @@ from operations.views import RoomView, ZoneView, UnitView, ChoicesZoneView
 
 app_name = 'operations'
 urlpatterns = [
-    path('room', RoomView.as_view()),
-    path('zone', ZoneView.as_view()),
-    path('unit', UnitView.as_view()),
+    re_path(r'zone/(?P<row_id>\d+)?', ZoneView.as_view()),
+    re_path(r'room/(?P<row_id>\d+)?', RoomView.as_view()),
+    re_path(r'unit/(?P<row_id>\d+)?', UnitView.as_view()),
     path('zone/choices', ChoicesZoneView.as_view()),
 ]
