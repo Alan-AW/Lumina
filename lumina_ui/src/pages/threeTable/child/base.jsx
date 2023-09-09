d138740a9368515c86dd839177f278973443995c
Alan-AW <xcdh560@foxmail.com>
2023-09-09 14:12:06 +0800

    数据表的新增对接完成

--- lumina_ui/src/pages/threeTable/child/base.jsx
+++ lumina_ui/src/pages/threeTable/child/base.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState, useMemo } from 'react'
-import { Table, Button, Popconfirm } from 'antd'
-import { useNavigate } from 'react-router-dom'
-import { DeleteOutlined, QuestionCircleOutlined, EditOutlined, RollbackOutlined } from '@ant-design/icons'
-import { FADEINRIGHT, pageSize } from 'contants'
+import { Table, Button, Popconfirm, notification, message } from 'antd'
+import { DeleteOutlined, QuestionCircleOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons'
+import dayjs from 'dayjs'
+import BaseModal from 'components/threeData/baseModal'
+import Action from './action'
+import { FADEIN, pageSize } from 'contants'
+import { getInstruction, postInstruction, patchInstruction, deleteInstruction } from 'network/api'
+import { openNotification } from 'utils'
 
-function Base() {
-  const navigate = useNavigate()
+function Base(props) {
+  const { phasesId } = props
+  const [api, contextHolder] = notification.useNotification()
   const [params, setparams] = useState({ page: 1 })
-  const [tableData, settableData] = useState([
-    { id: 1, name_en: 'name_en', name_cn: 'name_cn', description_en: 'description_en', description_cn: 'description_cn' }
-  ])
-  const [tableDataCount, settableDataCount] = useState(1)
+  const [tableData, settableData] = useState([])
+  const [tableDataCount, settableDataCount] = useState(0)
+  const [baseId, setbaseId] = useState(null)
   const tableTitle = [
     {
       title: '序号',
@@ -23,24 +27,39 @@ function Base() {
       dataIndex: 'id'
     },
     {
-      title: 'name_en',
+      title: 'status',
       align: 'center',
-      dataIndex: 'name_en'
+      dataIndex: 'status'
     },
     {
-      title: 'name_cn',
+      title: 'type',
       align: 'center',
-      dataIndex: 'name_cn'
+      dataIndex: 'type'
     },
     {
-      title: 'description_en',
+      title: 'n-weeks',
       align: 'center',
-      dataIndex: 'description_en'
+      dataIndex: 'n_weeks'
     },
     {
-      title: 'description_cn',
+      title: 'dow',
       align: 'center',
-      dataIndex: 'description_cn'
+      dataIndex: 'dow'
+    },
+    {
+      title: 'tod',
+      align: 'center',
+      dataIndex: 'tod'
+    },
+    {
+      title: 'interval',
+      align: 'center',
+      dataIndex: 'interval'
+    },
+    {
+      title: 'duration',
+      align: 'center',
+      dataIndex: 'duration'
     },
     {
       title: '操作',
@@ -68,26 +87,95 @@ function Base() {
               icon={<DeleteOutlined />}
             />
           </Popconfirm>
+          <Button
+            type='link'
+            children='action↓'
+            onClick={() => setbaseId(row.id)}
+          />
         </div>
       )
     }
   ]
+  const [openModal, setopenModal] = useState(false)
+  const [editInitValue, seteditInitValue] = useState(null)
+  const [isEdit, setisEdit] = useState(false)
 
-  // useEffect(() => {
-  //   getBaseData(params).then(res => {
-  //     if (res.status) {
-  //       settableData(res.data.results)
-  //       settableDataCount(res.data.count)
-  //     }
-  //   }).catch(err => console.log(err))
-  // }, [params])
+  useEffect(() => {
+    if (!!phasesId) {
+      getInstruction(phasesId, params).then(res => {
+        if (res.status) {
+          settableData(res.data.results)
+          settableDataCount(res.data.count)
+        }
+      }).catch(err => console.log(err))
+    }
+  }, [phasesId, params])
 
   const deleteRow = row => {
-    console.log(row)
+    deleteInstruction(row.id).then(res => {
+      if (res.status) {
+        settableData(tableData.filter(item => item.id !== res.data))
+        settableDataCount(tableDataCount - 1)
+        message.success(res.info)
+      } else {
+        message.error(res.errs)
+      }
+    }).catch(err => console.log(err))
+  }
+
+  const addClick = () => {
+    setisEdit(false)
+    setopenModal(true)
   }
 
   const editClick = row => {
-    console.log(row)
+    setisEdit(true)
+    if (row.type === 'timed') {
+      row.tod = dayjs(row.tod, 'HH:mm:ss')
+    } else {
+      row.interval = dayjs(row.interval, 'HH:mm:ss')
+      row.duration = dayjs(row.deration, 'HH:mm:ss')
+    }
+    seteditInitValue(row)
+    setopenModal(true)
+  }
+
+  const onOk = value => {
+    value.phases = phasesId
+    if (isEdit) {
+      patchInstruction(value.id, value).then(res => {
+        if (res.status) {
+          settableData(tableData.map(item => {
+            if (item.id === res.data.id) {
+              item = res.data
+            }
+            return item
+          }))
+          message.success(res.info)
+          closeModal()
+        } else {
+          openNotification(api, 'error', res.errs)
+        }
+      }).catch(err => console.log(err))
+    } else {
+      delete value.id
+      postInstruction(value).then(res => {
+        if (res.status) {
+          settableData([res.data, ...tableData])
+          settableDataCount(tableDataCount + 1)
+          message.success(res.info)
+          closeModal()
+        } else {
+          openNotification(api, 'error', res.errs)
+        }
+      })
+    }
+  }
+
+  // 关窗
+  const closeModal = () => {
+    seteditInitValue(null)
+    setopenModal(false)
   }
 
   // 分页
@@ -100,7 +188,8 @@ function Base() {
   // 表格
   const table = useMemo(() => (
     <Table
-      className={FADEINRIGHT}
+      title={() => 'Base'}
+      className={FADEIN}
       dataSource={tableData}
       columns={tableTitle}
       pagination={paginationProps}
@@ -112,13 +201,23 @@ function Base() {
 
   return (
     <>
+      {contextHolder}
       <Button
-        onClick={() => navigate('/phases')}
-        icon={<RollbackOutlined />}
         type='primary'
+        icon={<PlusOutlined />}
+        children='添加'
+        onClick={addClick}
+        disabled={!phasesId}
         style={{ marginBottom: 'var(--content-margin)' }}
-      >返回上级</Button>
+      />
       {table}
+      <BaseModal
+        initValue={editInitValue}
+        openModal={openModal}
+        closeModal={closeModal}
+        onOk={onOk}
+      />
+      <Action baseId={baseId} />
     </>
   )
 }
